86c8763440b3d8b05df9539e55c01719354d53cb
gcd <gael.cougnaud@samsic.fr>
2024-01-18 17:42:01 +0100

    - bump expo to 50.0.1 - bump RN to 0.73.2

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "preset": "react-native",
+  "moduleFileExtensions": [
+    "js",
+    "jsx",
+    "json",
+    "ts",
+    "tsx",
+  ],
+  "transform": {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  },
+  "testMatch": [
+    "**/*.test.ts?(x)"
+  ]
+};
