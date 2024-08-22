86e3399f8b13a0c2b3d6a450169f6f4d4f8b7054
gcd <gael.cougnaud@samsic.fr>
2024-08-22 11:34:44 +0200

    prepare 2.3.1

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: 'jest-expo',
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   transform: {
-    '\\.[jt]sx?$': 'babel-jest'
+    '\\.[jt]sx?$': 'babel-jest',
   },
-  "transformIgnorePatterns": [
-    "/node_modules/(?!react-native)/.+"
+  transformIgnorePatterns: [
+    '/node_modules/(?!react-native)/.+',
   ],
   testMatch: ['**/*.test.ts?(x)'],
 };
